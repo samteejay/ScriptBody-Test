6467b8048019d95b8ea62f1b90325f31584104be
Samuel Tijani <omalitijani@gmail.com>
2020-02-10 14:58:16 +0100

    Init commit

---
+++ scriptbodytest/scriptbodytest.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Testing JavaScript in the Body Section</title>
+</head>
+<body>
+<h1>This is the web page</h1>
+<script>
+alert("This is the JavaScript program!");
+</script>
+<h1>This is the end of the web page</h1>
+</body>
+<html>
